e13a5819e000776b1fb808f37aa4e328a3494c0e
ciaranj <ciaranj@gmail.com>
2014-03-26 10:41:28 +0000

    Adds support for multiple metrics/wildcards to average function * Probably needs propagating across a bunch of the method and simplifying down.

--- test/ServerTests/AvgTests.js
+++ test/ServerTests/AvgTests.js
@@ -46,7 +46,7 @@ describe('TargetParseContext', function(){
         TargetParser.parse( metric )(ctx)
                     .then(function (result) {
                             assert.equal( 1, result.length );
-                            assert.deepEqual( [1,,3,null,5,6], result[0].data.values );
+                            assert.deepEqual( [1,,3,,5,6], result[0].data.values );
                             done();
                     })
                     .end();
@@ -90,6 +90,18 @@ describe('TargetParseContext', function(){
                     .then(function (result) {
                             assert.equal( 1, result.length );
                             assert.equal( "avg(foo.[2])", result[0].name );
+                            done();
+                    })
+                    .end();
+    })    
+    it('should correctly handle multiple seperate metrics', function(done) {
+        var metric=  "avg(foo.1,foo.{bar,tar},foo.2)";
+        var ctx= Utils.buildTargetParseContext( metric,  [new MetricInfo("foo.1"), new MetricInfo("foo.2"), new MetricInfo("foo.bar"), new MetricInfo("foo.tar")], {"foo.1":[1,2,3,4], "foo.2":[10,20,30,50],"foo.bar":[1,2,3,4], "foo.tar":[10,20,30,50]} );
+        TargetParser.parse( metric )(ctx)
+                    .then(function (result) {
+                            assert.equal( 1, result.length );
+                            assert.deepEqual( [5.5,11,16.5,27], result[0].data.values );
+                            assert.equal( "avg(foo.1,foo.{bar,tar},foo.2)", result[0].name );
                             done();
                     })
                     .end();
